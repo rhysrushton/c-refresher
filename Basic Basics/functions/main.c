3591cd19295b0c8e1e044f70f17e3b21b4c4c5e1
rhysrushton <rhysworker@gmail.com>
2022-05-09 10:12:19 +1000

    hey

---
+++ Basic Basics/functions/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+  
+  //think abut functions as a black box
+  // When designing code don't worry about writing function code
+  // just think about what the function is supposed to do 
+  /* Example of a Function 
+     Return_type Function_name (Paramters - seperated by commas)
+     {
+      Statements...
+     }
+  */
+  // first line of the function definition tells the compiler:
+  // the type of value the function returns
+  // the name of the function
+  // the arguments it takes
+  /* example function 
+     void printMessage (void) {
+     printf("Programming is fun. \n"); 
+     }
+     The first line of the printMessage() function definition tells the compiler that the
+     function returns no value -> keyword void.
+     next is the nameof the function: printMessage
+     after that it takes no arguments (second use of the keyword void)
+  */ 
+   
+  
+  return 0; 
+}
